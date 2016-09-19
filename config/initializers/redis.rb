d04fb4279fcf014e33837e46f4ab60ddc510c852
安達健二 <kenji.183927@gmail.com>
2016-09-19 21:51:14 +0900

    forheroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 require 'redis'
 
 uri = URI.parse(ENV["REDISCLOUD_URL"])
-REDIS = Redis.new(host: uri.host, port: uri.port)
+REDIS = Redis.new(host: uri.host, port: uri.port, :password => uri.password)
